a251c51356024d5d5ae4dc7a7cd95df365d71a5e
Martin Walsh <github@mwalsh.org>
2012-04-05 10:11:00 -0500

    text version added, not great, but close enough

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -4,6 +4,7 @@ set -e
 TEXLIVE_BIN=/media/depot/workspace/texlive/2011/bin/x86_64-linux
 
 IN=$1
+LABEL=${IN%.tex}
 
 if [ -z "$IN" ]; then
   echo "$0: input.tex"
@@ -12,15 +13,32 @@ fi
 
 PATH=$TEXLIVE_BIN:$PATH
 
+# latex2html settings
+LATEX=$TEXLIVE_BIN/latex
+
 # The long way 'round
 #latex $IN
-#dvips ${IN%.tex}.dvi
-#dvipdf ${IN%.tex}.dvi
+#dvips $LABEL.dvi
+#dvipdf $LABEL.dvi
 
 pdflatex $IN
 
-pdftk ${IN%.tex}.pdf cat 2-1 output MartinWalsh.pdf
+### latex2html (scrapped)
+### 1 html file, no 'about this document' section
+## latex2html -split +0 -info 0 -no_navigation $IN
+
+### convert to txt
+## lynx -dump "$LABEL/index.html" > $LABEL.txt
+
+## rm -r $LABEL/
+
+# plain text version
+latex $IN
+catdvi -e 1 -U $LABEL.dvi | sed -re "s/\[U\+22C6\]/*/g" | sed -re "s/([^^[:space:]])\s+/\1 /g" |  sed -re "s/\[U\+2709\]/                                                                                  /g" | sed -re "s/\[U\+00E9\]/e/g"  > $LABEL.txt
+
+# reorder/organize output
+pdftk $LABEL.pdf cat 2-1 output MartinWalsh.pdf
 pdftk MartinWalsh.pdf cat 1 output MartinWalshCoverLetter.pdf
-pdftk MartinWalsh.pdf cat 1 output MartinWalshResume.pdf
+pdftk MartinWalsh.pdf cat 2 output MartinWalshResume.pdf
 
 exit 0
