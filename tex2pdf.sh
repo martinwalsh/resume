b3bbd790d8e96b4291aa1e5d3ab65c0f533cdb49
Martin Walsh <github@mwalsh.org>
2012-04-04 20:44:51 -0500

    template final

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 TEXLIVE_BIN=/media/depot/workspace/texlive/2011/bin/x86_64-linux
 
@@ -11,9 +12,15 @@ fi
 
 PATH=$TEXLIVE_BIN:$PATH
 
+# The long way 'round
 #latex $IN
 #dvips ${IN%.tex}.dvi
 #dvipdf ${IN%.tex}.dvi
+
 pdflatex $IN
+
+pdftk ${IN%.tex}.pdf cat 2-1 output MartinWalsh.pdf
+pdftk MartinWalsh.pdf cat 1 output MartinWalshCoverLetter.pdf
+pdftk MartinWalsh.pdf cat 1 output MartinWalshResume.pdf
 
 exit 0
